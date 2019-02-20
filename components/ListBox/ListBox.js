a9dc39ee30d4915fc97bf6735c0e6166a6ad28c6
Marcamillian <marcamillian@gmail.com>
2019-02-20 12:11:49 +0000

    feat: Add scrolling to the list-box
    - Add more items to index to allow the scroll
    - Added container and slot to list-box to scroll
    - Added styling to hide the scrollbars in webkit
    - Add scroll-behaviour: smooth to the container
    - Add additional mouse listeners to scroll by mouse movement

--- components/ListBox/ListBox.js
+++ components/ListBox/ListBox.js
@@ -20,29 +20,43 @@ class ListBox extends HTMLElement{
   slice(nodeList){
     return Array.prototype.slice.call(nodeList)
   }
+  
 
   connectedCallback(){
     let listName = this.getAttribute('list-name') || 'Some list';
     let listOrientation = this.getAttribute('list-orientation') || 'vertical';
-    
+
+    // set attributes on the main container
     this.setAttribute('tabindex',0);
     this.setAttribute("role", "listbox");
     this.setAttribute("aria-label", listName);
     this.setAttribute("aria-orientation", listOrientation)
 
+    // deal with the template
+    let shadowRoot = this.attachShadow({mode:'open'});
+    let template = currentDocument.querySelector('#list-box-template');
+    let instance = template.content.cloneNode(true);
+
+    shadowRoot.appendChild(instance);
+
+    // handle the list of options that we have
     this.items = this.slice(this.children);
     this.items.forEach(item =>{
       item.setAttribute("role","option")
       item.addEventListener("focusout", this.blurItem.bind(item))
+      item.style.display = "inline-block";
     })
     
     this.focusedIdx = 0; // default focused element
     this.focusedItem= this.items[this.focusedIdx] // set ref to focused item
+    this.scrollOffset = 0;
+
 
     this.addEventListener('keydown', this.handleKeyDown.bind(this))
     //this.addEventListener('focus', this.handleOnFocus.bind(this))
-    this.addEventListener('mousedown', this.handleOnClick.bind(this))
-
+    this.addEventListener('mousedown', this.handleClickStart.bind(this))
+    this.addEventListener('mouseup', this.handleClickEnd.bind(this))
+    this.addEventListener('mousemove', this.handleMouseMove.bind(this))
   }
 
   handleKeyDown(event){
@@ -81,10 +95,39 @@ class ListBox extends HTMLElement{
     this.changeFocus(this.focusedIdx)
   }
 
-  handleOnClick(event){
-    event.preventDefault()
-    let clickedItemIndex = this.items.indexOf(event.target);
-    this.changeFocus(clickedItemIndex)
+  handleClickStart(event){
+    
+    if(event.buttons == 1){
+      console.log("Click down")
+      event.preventDefault()
+
+      // define some kind of start point 
+
+      //
+      
+
+    }
+  }
+
+  handleClickEnd(event){
+    console.log("Click up")
+    if (this.items.includes(event.target)){
+      event.preventDefault()
+
+      let clickedItemIndex = this.items.indexOf(event.target);
+      this.changeFocus(clickedItemIndex)
+    }
+  }
+
+  handleMouseMove(event){
+    if(event.buttons == 1){
+      event.preventDefault()
+      
+      // scroll some 
+
+      //
+
+    }
   }
 
 
